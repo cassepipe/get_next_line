8643c065f332acd104fbd1eb45bb0382c8778a92
cassepipe <cassepipe@ymail.com>
2020-08-15 09:48:29 +0200

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpouget <cassepipe@ymail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/08 16:20:06 by tpouget           #+#    #+#             */
+/*   Updated: 2020/08/13 17:54:33 by tpouget          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 1024
+# endif
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strjoin(char *s1, char *s2);
+int		get_next_line(int fd, char **line);
+
+#endif
---
+++ gnl.c
@@ -0,0 +1,47 @@
+#include "get_next_line.h"
+#include <string.h>
+/*char *	find_newline_or_read(char **buffer, size_t *bufsize)*/
+/*{*/
+/*}*/
+
+
+int	get_next_line(int fd, char **line)
+{
+	static char	*buffer;
+	char		*temp;
+	char		*newline;
+	ssize_t		bytes_read;
+	size_t 	bufsize;
+	size_t 	line_size;
+
+	if (fd < 0 || BUFFER_SIZE < 1
+		|| (!buffer && (!(buffer = calloc(sizeof(char), BUFFER_SIZE + 1)))))
+		return (-1);
+	line_size = 0;
+	bufsize = strlen(buffer) + 1;
+	while(!(newline = strchr(buffer, '\n')))
+	{
+		temp = malloc(BUFFER_SIZE + 1);
+		if(!(bytes_read = read(fd, temp, BUFFER_SIZE)))
+			break;
+		else if (bytes_read < 0)
+			return (-1);
+		buffer[BUFFER_SIZE] = '\0';
+		if(!(buffer = ft_strjoin(buffer, temp)))
+			return (-1);
+		free(temp);
+		bufsize += bytes_read;
+	}
+	line_size = bufsize;
+	if (newline)
+		line_size -= BUFFER_SIZE + (newline - buffer);
+	temp = *line;
+	if (!(*line = strndup(buffer, line_size)))
+		return (-1);
+	free(temp);
+	temp = buffer;
+	if (!(buffer = strndup(++newline, bufsize - line_size))) //what if newline is NULL ?
+		return (-1);
+	free(temp);
+	return (newline ? 0 : 1);
+}
