5828d7ddc24de1b74618b869ffc16a20477114c7
cassepipe <cassepipe@ymail.com>
2020-08-18 11:42:55 +0200

    Passing GNL Lover

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,5 @@
 #include "get_next_line.h"
 #include <string.h>
-#include "printbuffer.c"
 #include <stdio.h>
 
 
@@ -15,9 +14,15 @@ static int grow_buffer_until_newline(int fd, char **buffer,
 	{
 		new_buffer = malloc(BUFFER_SIZE + 1);
 		if(!(bytes_read = read(fd, new_buffer, BUFFER_SIZE)))
-			break;
+		{
+			free(new_buffer);
+			return (1);
+		}
 		else if (bytes_read < 0)
+		{
+			free(new_buffer);
 			return (0);
+		}
 		new_buffer[bytes_read] = '\0';
 		old_buffer = *buffer;
 		if(!(*buffer = ft_strjoin(old_buffer, new_buffer)))
@@ -27,8 +32,6 @@ static int grow_buffer_until_newline(int fd, char **buffer,
 		*bufsize += bytes_read;
 		/*printbuffer(*buffer, *bufsize);*/
 	}
-	if (!bytes_read)
-		free(new_buffer);
 	return (1);
 }
 
@@ -63,84 +66,12 @@ int	get_next_line(int fd, char **line)
 		return (-1);
 
 	//Recupere le reste du buffer apres le \n pour le prochain appel
+	temp = buffer;
 	if (!(buffer = ft_strndup(newline + 1, bufsize - line_len))) //hypothesis : if !newline, line_len = bufsize, so we have strndup(nl, 0) --> returns empty string
 		return (-1);
+	free(temp);
 
 	if (!newline)
 		free(buffer);
 	return (newline ? 1 : 0);
 }
-
-/*int main(void)*/
-/*{*/
-	/*char *line = NULL;*/
-	/*FILE *file;*/
-	/*file = fopen("input.txt", "r");*/
-	/*int fd = fileno(file);*/
-	/*int ret;*/
-
-	/*for (int i = 0;  i < 15; i++)*/
-	/*{*/
-		/*ret = get_next_line(fd, &line);*/
-		/**line ? printf("%s\n", line) : printf("Empty string\n");*/
-		/*printf("Function returned %d\n", ret);*/
-	/*//	free(line);*/
-	/*}*/
-	/*fclose(file);*/
-	/*return (0);*/
-/*}*/
-
-int main(void)
-{
-
-	FILE *dest =  fopen("output.txt", "w");
-
-	char *line = NULL;
-	FILE *src = fopen("input.txt", "r");
-	int fd = fileno(src);
-
-	while (get_next_line(fd, &line))
-	{
-		/*printf("%s\n", line);*/
-		fprintf(dest, "%s\n", line);
-		free(line);
-		line = NULL;
-}
-	/*printf("%s", line);*/
-	fprintf(dest, "%s", line);
-	free(line);
-
-	fclose(src);
-	return (0);
-}
-
-/*int main(void)*/
-/*{*/
-	/*FILE *dest =  fopen("output.txt", "a");*/
-
-	/*static char *lines[3];*/
-	/*FILE *src1 = fopen("1.txt", "r");*/
-	/*FILE *src2 = fopen("2.txt", "r");*/
-	/*FILE *src3 = fopen("3.txt", "r");*/
-	/*int fds[3];*/
-	/*fds[0] = fileno(src1);*/
-	/*fds[1] = fileno(src2);*/
-	/*fds[2] = fileno(src3);*/
-
-	/*for (int i = 0;  i < 3; i++)*/
-	/*{*/
-		/*int j = 0;*/
-		/*while(j <= i)*/
-		/*{*/
-			/*get_next_line(fds[i], &lines[j]);*/
-			/*j++;*/
-		/*}*/
-		/*[>printf("%s\n", lines[i]);<]*/
-		/*fprintf(dest, "%s\n", lines[i]);*/
-	/*}*/
-	/*fclose(src1);*/
-	/*fclose(src2);*/
-	/*fclose(src3);*/
-	/*fclose(dest);*/
-/*}*/
-
